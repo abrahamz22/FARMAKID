19616ee63db23b1bed0dfb1c0d4817fe455c6b04
abrahamz22 <abrahamz22@ilg.cat>
2024-02-01 19:36:47 +0100

    añadido conexion

---
+++ php/connexionBaseDeDatos.php
@@ -0,0 +1,11 @@
+<?php
+$host = "localhost";//
+$database = "bbddpharmakid";//pongo el nombre de la base de datos
+$user = "usuarioAbraham";//mi usuario
+$password = "12345678";//mi contraseña de la base de datos
+$conexion = mysqli_connect($host, $user, $password,$database);
+
+if (!$conexion) die("No ha podido realizarse la conexión".mysqli_connect_error());//si no la concexion no es posible, sale ujn mensaje de no se ha podido conectar
+else 
+    echo "<script>alert('base de datos conectada')</script>"
+?>
