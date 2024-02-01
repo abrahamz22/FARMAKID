f6e3c9ee3b2842e88a27e8dbff91e16ab09a3350
abrahamz22 <abrahamz22@ilg.cat>
2024-02-01 18:25:19 +0100

    sds

--- js/aviso-cookies.js
+++ js/aviso-cookies.js
@@ -4,15 +4,23 @@ let avisoCookies = document.getElementById("cookies");
 
 let fondoAvisoCookies = document.getElementById("fondo-aviso-cookies");
 
+dataLayer = [];//variable global
+
+//condicional para acceder al local-storage.getItem , obtener el elemento cookies-aceptadas
 if(!localStorage.getItem("cookies-aceptadas")){
     avisoCookies.classList.add("activo");
     fondoAvisoCookies.classList.add("activo");
 
+}else{
+    dataLayer.push({'event': 'cookies aceptadas'});//con este agregar un objeto, que es un evento que se llama cookies aceptadas 
 }
 
+//cuando da click al boton de aceptar cookies 
 botonAceptarCookie.addEventListener("click", () =>{
     avisoCookies.classList.remove("activo");
     fondoAvisoCookies.classList.remove("activo");
 
     localStorage.setItem("cookies-aceptadas", true);
+
+    dataLayer.push({'event': 'cookies aceptadas'});//con este agregar un objeto, que es un evento que se llama cookies aceptadas 
 });
