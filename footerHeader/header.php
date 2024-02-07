0371bf3c03d9d3a1cb0b88934f99205ce2f9f025
abrahamz22 <abrahamz22@ilg.cat>
2024-02-07 17:40:16 +0100

    cambios post: falta quitar el divplegable de los php pero ya esta en el header

--- footerHeader/header.php
+++ footerHeader/header.php
@@ -54,4 +54,24 @@
             </div>
         </nav>
         <!--/NAV-->
+
+
+      
     </header>
+
+        <!--MENU DESPLEGABLE MEDICAMENTOS-->
+        <div id="desplegableMedicamentos" class="removeId">
+        <ul id="listaD">
+            <li><a href="medicamentos-interaccion.php">Interecciones entre medicamentos</a></li>
+            <li><a href="medicamentos-retirados.php">Medicamentos retirados</a></li>
+        </ul>
+    </div>
+    <!--/MENU DESPLEGABLE MEDICAMENTOS-->
+    <!--MENU DESPLEGABLE MEDICAMENTOS-->
+    <div id="desplegableProteinas" class="removeId">
+        <ul id="listaD">
+          
+            <li><a href="info-proteinas.php">Información general sobre las proteínas</a></li>
+        </ul>
+    </div>
+    <!--/MENU DESPLEGABLE MEDICAMENTOS-->
